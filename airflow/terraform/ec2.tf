0b269785eef831d4cdf00860af7ab47c9c11b033
Alvis <alviswhyi@gmail.com>
2026-09-08 22:50:51 +0800

    Change secret to variable

--- airflow/terraform/ec2.tf
+++ airflow/terraform/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "airflow" {
   ami           = var.airflow_ami_id
   instance_type = var.ec2_parameters["instance_type"]
 
-  subnet_id = var.private_subnet_ids[0]
+  subnet_id = var.public_subnet_ids[0]
 
   vpc_security_group_ids = [
     aws_security_group.airflow_ec2_sg.id
